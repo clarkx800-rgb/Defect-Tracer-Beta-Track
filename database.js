f61bb92996e5b54a6602c73748edd19ba26b1305
clarkx800-rgb <clarkx800@gmail.com>
2026-04-07 11:48:10 -0700

    Update database.js

--- database.js
+++ database.js
@@ -495,16 +495,16 @@ const globalDatabase = {
             "LHZ-BQZ-OB": {
                 name: "LHZ-BQZ-OB DC201-200",
                 segments: [
-                    { start: 7896, end: 7897, color: "#CC3752" }
+                    { start: 7896, end: 7897, color: "#37CCB1" }
                 ]
             },
-            "LHZ-BQZ-OB": {
+            "LHZ-BQZ-OB-X1": {
                 name: "LHZ-BQZ-OB DC202-203",
                 segments: [
                     { start: 7895, end: 7893, color: "#E7A2AE" }
                 ]
             },            
-            "BQZ-NPZ-OB": {
+            "BQZ-NPZ-OB-X": {
                 name: "BQZ-NPZ-OB",
                 segments: [
                     { start: 7773, end: 7686, color: "#9DCC37" }
@@ -516,19 +516,19 @@ const globalDatabase = {
                     { start: 7685, end: 7545, color: "#37CCB1" }
                 ]
             },
-            "NPZ-FCZ-OB": {
+            "NPZ-FCZ-OB-X1": {
                 name: "NPZ-FCZ-OB DC205-204",
                 segments: [
                     { start: 7890, end: 7892, color: "#3752CC" }
                 ]
             },
-            "NPZ-FCZ-OB": {
+            "NPZ-FCZ-OB-X2": {
                 name: "NPZ-FCZ-OB DC206-207",
                 segments: [
                     { start: 7889, end: 7887, color: "#CC3752" }
                 ]
             },
-            "NPZ-FCZ-OB": {
+            "NPZ-FCZ-OB-X3": {
                 name: "NPZ-FCZ-OB DC214-215",
                 segments: [
                     { start: 7685, end: 7545, color: "#37CCB1" }
@@ -540,43 +540,43 @@ const globalDatabase = {
                     { start: 7544, end: 7421, color: "#6637CC" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X1": {
                 name: "FCZ-LAT-OB DC219-218",
                 segments: [
                     { start: 7861, end: 7862, color: "#CC6637" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X2": {
                 name: "FCZ-LAT-OB DC221-222",
                 segments: [
                     { start: 7860, end: 7860, color: "#CCB137" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X3": {
                 name: "FCZ-LAT-OB DC220-RH Spur End",
                 segments: [
                     { start: 7857, end: 7852, color: "#37CC66" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X4": {
                 name: "FCZ-LAT-OB DC223-LH Spur End",
                 segments: [
                     { start: 7850, end: 7848, color: "#52CC37" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X5": {
                 name: "FCZ-LAT-OB DC223-LH Spur End",
                 segments: [
-                    { start: 7850, end: 7848, color: "#52CC37" }
+                    { start: 7850, end: 7848, color: "#D4596F" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X6": {
                 name: "FCZ-LAT-OB DC225-224",
                 segments: [
                     { start: 7845, end: 7847, color: "#3752CC" }
                 ]
             },
-            "FCZ-LAT-OB": {
+            "FCZ-LAT-OB-X7": {
                 name: "FCZ-LAT DC226-227",
                 segments: [
                     { start: 7844, end: 7840, color: "#CC3752" }
