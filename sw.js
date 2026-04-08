9abd6c2b094da9e25675c882c1364671e871aa68
clarkx800-rgb <clarkx800@gmail.com>
2026-04-08 07:47:52 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // This is the permanent Service Worker for V2
-const CACHE_NAME = 'defect-tracker-v2-prod-1.2.36'; 
+const CACHE_NAME = 'defect-tracker-v2-prod-1.2.37'; 
 
 const ASSETS_TO_CACHE = [
   './',
