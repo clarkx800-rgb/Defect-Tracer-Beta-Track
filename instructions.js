55a07f8a74f1dabedd75a5c2808b0ace4b6f34f0
clarkx800-rgb <clarkx800@gmail.com>
2026-04-08 17:05:26 -0700

    Update instructions.js

--- instructions.js
+++ instructions.js
@@ -5,6 +5,7 @@ const systemInstructions = `
             <li>Open the top right menu (✖/⚙).</li>
             <li>Select <b>1. LINE</b>, <b>2. DIRECTION</b> (IB or OB), and <b>3. SUB-TO-SUB AREA</b>.</li>
             <li>Tap <b>LOAD AREA</b> to generate your inspection route.</li>
+                <b>TIP:</b> Load all needed sub to sub areas in normal order before flipping direction if the start of inpsection on the opposite side.
         </ul>
 
         <p style="color: var(--text-color); font-weight: bold; text-transform: uppercase;">STEP 2: LOG DEFECTS</p>
@@ -15,16 +16,17 @@ const systemInstructions = `
             <li>Tap <b>📷 PHOTO</b> to attach visual evidence (Multiple allowed).</li>
         </ul>
 
-        <p style="color: var(--text-color); font-weight: bold; text-transform: uppercase;">STEP 3: TACTICAL TOOLS</p>
+        <p style="color: var(--text-color); font-weight: bold; text-transform: uppercase;">STEP 3: INSERT POCKET TRACK (OB ONLY)</p>
         <ul style="margin-top: 5px; padding-left: 20px; margin-bottom: 20px;">
-            <li><b>LONG PRESS</b> any TS on the mini-map to Splice/Insert a switch.</li>
-            <li>Tap the <b>Floating + Button</b> to quick-add continuous routes.</li>
+            <li><b>LONG PRESS</b> desired TS on the mini-map to Insert a pocket track TS numbers.</li>
+            <li>Tap the <b>Floating "+" Button</b> to quick-add track section series for pocket.</li>
+            <li>Pocket tracks can be found in OUTBOUND drop-down menu</li>
         </ul>
 
         <p style="color: var(--warning); font-weight: bold; text-transform: uppercase;">STEP 4: SAVE DATA ⚠️</p>
         <ul style="margin-top: 5px; padding-left: 20px;">
             <li>System does not auto-save. Open menu and tap <b style="color: var(--warning)">💾 SAVE DATA</b> frequently.</li>
-            <li>Tap <b style="color: var(--success)">📊 EXPORT REPORT</b> when the shift is complete to generate your Excel & PDF files.</li>
+            <li>Tap <b style="color: var(--success)">📊 EXPORT REPORT</b> at the end of inspection to generate your data collected to Excel & PDF picture files.</li>
         </ul>
     </div>
 `;
