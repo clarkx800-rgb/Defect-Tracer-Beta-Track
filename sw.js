a53fbb778e65be4e026d62d8e2bccd5065a2ba8a
clarkx800-rgb <clarkx800@gmail.com>
2026-04-09 14:04:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change version number to force users' phones to update!
-const CACHE_NAME = 'defect-tracker-v2-prod-2.0.86'; 
+const CACHE_NAME = 'defect-tracker-v2-prod-2.0.87'; 
 
 const ASSETS_TO_CACHE = [
   './',
