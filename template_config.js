19275342d85d90a55ee46565585791adb8d486dd
clarkx800-rgb <clarkx800@gmail.com>
2026-04-10 18:27:58 -0700

    Update template_config.js

--- template_config.js
+++ template_config.js
@@ -1,1 +1,15 @@
 
+const templateLibrary = {
+    'LIM_WALKS': [
+        { name: 'BW-MTS-SP-WALK', url: 'templates/lim_walks/BW-MTS-SP-WALK.json' }
+    ],
+    'SPEEDER_WALKS': [
+        { name: 'Placeholder Speeder Walk', url: 'templates/speeder_walks/SPEEDER-PLACEHOLDER.json' }
+    ],
+    'POWER_RAIL': [
+        { name: 'Placeholder Power Rail', url: 'templates/power_rail/POWER-PLACEHOLDER.json' }
+    ],
+    'SWITCH': [
+        { name: 'Placeholder Switch', url: 'templates/switch/SWITCH-PLACEHOLDER.json' }
+    ]
+};
