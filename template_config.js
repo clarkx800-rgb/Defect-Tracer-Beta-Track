0d56d09b2708df3044eab43956e24435aca38e18
clarkx800-rgb <clarkx800@gmail.com>
2026-04-10 18:58:27 -0700

    Update template_config.js

--- template_config.js
+++ template_config.js
@@ -4,14 +4,13 @@ const templateLibrary = {
         { name: 'BW-MTS-SP-WALK', url: 'templates/lim_walks/BW-MTS-SP-WALK.json' }
     ],
     'SPEEDER_WALKS': [
-        { name: 'SELECT YOUR SP-WALK ROUTE', url: 'templates/speeder_walks/SPEEDER-PLACEHOLDER.json' },
         { name: 'WF-BW-OB-SP-WALK', url: 'templates/speeder_walks/WF-BW-OB-SP-WALK.json' },
         { name: 'BW-MTS-SP-WALK', url: 'templates/speeder_walks/BW-MTS-SP-WALK.json' }        
     ],
     'POWER_RAIL': [
-        { name: 'SELECT PR-INSP ROUTE', url: 'templates/power_rail/POWER-PLACEHOLDER.json' }
+        // { name: 'SELECT PR-INSP ROUTE', url: 'templates/power_rail/POWER-PLACEHOLDER.json' }
     ],
     'SWITCH': [
-        { name: 'SELECT SW GROUP', url: 'templates/switch/SWITCH-PLACEHOLDER.json' }
+        // { name: 'SELECT SW GROUP', url: 'templates/switch/SWITCH-PLACEHOLDER.json' }
     ]
 };
